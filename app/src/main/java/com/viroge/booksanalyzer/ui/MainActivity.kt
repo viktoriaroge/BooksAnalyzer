97e8685b9caec3e58b1c02ba34810f59dc5f3ddd
Viktoria Georgieva <viktoria.ro.ge@gmail.com>
2026-03-22 00:07:19 +0200

    BUG: On app recreation, the splash was stealing the show

--- app/src/main/java/com/viroge/booksanalyzer/ui/MainActivity.kt
+++ app/src/main/java/com/viroge/booksanalyzer/ui/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.viroge.booksanalyzer.R
 import com.viroge.booksanalyzer.ui.nav.AppRoot
 import com.viroge.booksanalyzer.ui.theme.BooksAnalyzerTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,35 +25,39 @@ class MainActivity : ComponentActivity() {
     private val startupViewModel: StartupViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Install splash screen:
-        val splashScreen = installSplashScreen()
-        splashScreen.setKeepOnScreenCondition {
-            startupViewModel.isLoading.value.also { Log.d("MainActivity", "STARTUP: isLoading: $it") }
-        }
-        splashScreen.setOnExitAnimationListener { splashScreenView ->
-            // Try to get the splash center icon and do a fade out animation:
-            try {
-                splashScreenView.iconView
-            } catch (_: Exception) {
-                null
-            }?.let { iconView ->
-                val scaleX = ObjectAnimator.ofFloat(iconView, View.SCALE_X, 1f, 5f)
-                val scaleY = ObjectAnimator.ofFloat(iconView, View.SCALE_Y, 1f, 5f)
-                val alpha = ObjectAnimator.ofFloat(splashScreenView.view, View.ALPHA, 1f, 0f)
-
-                AnimatorSet().apply {
-                    duration = 400L
-                    playTogether(scaleX, scaleY, alpha)
-                    doOnEnd { splashScreenView.remove() }
-                    start()
-                }
-            } ?: {
-                splashScreenView.view.animate()
-                    .alpha(0f)
-                    .setDuration(400L)
-                    .withEndAction { splashScreenView.remove() }
-                    .start()
+        if (savedInstanceState == null) {
+            // Install splash screen:
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition {
+                startupViewModel.isLoading.value.also { Log.d("MainActivity", "STARTUP: isLoading: $it") }
             }
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                // Try to get the splash center icon and do a fade out animation:
+                try {
+                    splashScreenView.iconView
+                } catch (_: Exception) {
+                    null
+                }?.let { iconView ->
+                    val scaleX = ObjectAnimator.ofFloat(iconView, View.SCALE_X, 1f, 5f)
+                    val scaleY = ObjectAnimator.ofFloat(iconView, View.SCALE_Y, 1f, 5f)
+                    val alpha = ObjectAnimator.ofFloat(splashScreenView.view, View.ALPHA, 1f, 0f)
+
+                    AnimatorSet().apply {
+                        duration = 400L
+                        playTogether(scaleX, scaleY, alpha)
+                        doOnEnd { splashScreenView.remove() }
+                        start()
+                    }
+                } ?: {
+                    splashScreenView.view.animate()
+                        .alpha(0f)
+                        .setDuration(400L)
+                        .withEndAction { splashScreenView.remove() }
+                        .start()
+                }
+            }
+        } else {
+            setTheme(R.style.Theme_BooksAnalyzer_App)
         }
 
         enableEdgeToEdge(
